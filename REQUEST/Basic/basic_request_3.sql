545a047fd3de56993270db0fb2e696c63a1d53c1
Jonthan Rajao <127602265+Rjonathan03t@users.noreply.github.com>
2023-07-15 17:16:09 +0300

    Delete basic_request_3.sql

--- REQUEST/Basic/basic_request_3.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM reservation res
-INNER JOIN room r ON r.id_room = res.id_room
-INNER JOIN hotel h ON h.id_hotel = r.id_hotel
-WHERE h.id_hotel = id_hotel_given_by
-ORDER BY res.start_date_of_stay DESC;
