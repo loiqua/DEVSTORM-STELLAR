552534883df6b82b0b89f28387a02d94bfe9e868
Loiqua Andriamahaly <127602418+loiqua@users.noreply.github.com>
2023-07-15 16:05:16 +0200

    Delete Hard_request_11.sql

--- REQUEST/Hard_request_11.sql
+++
@@ -1,9 +0,0 @@
-SELECT room.room_number,
-       hotel.hotel_city
-FROM room
-JOIN hotel ON room.id_hotel = hotel.id_hotel
-WHERE room.id_room NOT IN
-        (SELECT reservation.id_room
-         FROM reservation
-         WHERE reservation.start_date_of_stay <= CURRENT_DATE + INTERVAL '1 week'
-             AND reservation.end_date_of_stay >= CURRENT_DATE + INTERVAL '1 week' )
